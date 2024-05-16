295103546af6533ed4fa26d886604821cc002588
aakshayy01 <akshay06454@gmail.com>
2024-05-16 19:30:29 +0530

    intial

---
+++ complete_react_2024/todo_project/todo_april/src/component/todo project/todo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./style.css"
+const todo = () => {
+  return (
+    <>
+      <div className="main-div">
+        <div className="child-div">
+            
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default todo
